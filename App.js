051f1153e27086392d25f2c64afafa487f71ff31
Cristian Guaman <cristian.guaman@icloud.com>
2020-10-25 15:03:16 -0500

    5. Lists 2. FlatList

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 export default function App() {
   return (
-    <ViewImageScreen />
+    <MessagesScreen />
   );
 }
