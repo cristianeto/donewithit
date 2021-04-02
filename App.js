17aef3cfcceb0b76a8912e97f184f27a7c71d53e
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-01 20:17:03 -0500

    6. Input Components 9. Showing Picker Items

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import Screen from "./app/components/Screen";
 import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
+
 export default function App() {
   return (
     <Screen>
-      <AppPicker icon="apps" placeholder="Category" />
+      <AppPicker icon="apps" items={categories} placeholder="Category" />
       <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -5,14 +5,16 @@ import {
   TouchableWithoutFeedback,
   Modal,
   Button,
+  FlatList,
 } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppText from "./AppText";
 
 import defaultStyles from "../config/styles";
 import Screen from "./Screen";
+import PickerItem from "./PickerItem";
 
-function AppPicker({ icon, placeholder, ...otherProps }) {
+function AppPicker({ icon, items, placeholder }) {
   const [modalVisible, setModalVisible] = useState("false");
 
   return (
@@ -38,6 +40,16 @@ function AppPicker({ icon, placeholder, ...otherProps }) {
       <Modal visible={modalVisible} animationType="slide">
         <Screen>
           <Button title="Close" onPress={() => setModalVisible(false)} />
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.value.toString()}
+            renderItem={({ item }) => (
+              <PickerItem
+                label={item.label}
+                onPress={() => console.log(item)}
+              />
+            )}
+          />
         </Screen>
       </Modal>
     </>
