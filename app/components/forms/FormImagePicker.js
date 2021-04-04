f229a3364ce29ca5ac417a8fb35a2a4fd61c269e
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-04 00:42:52 -0500

    2.Native Features 12. Building FormImagePicker

---
+++ app/components/forms/FormImagePicker.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+import { ErrorMessage } from "./ErrorMessage";
+import ImageInputList from "../ImageInputList";
+
+function FormImagePicker({ name }) {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  const imageUris = values[name];
+
+  const handleAdd = (uri) => {
+    setFieldValue([...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setFieldValue(imageUris.filter((imageUri) => imageUri !== uri));
+  };
+  return (
+    <>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
+
+export default FormImagePicker;
