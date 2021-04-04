8023e36de73f8a473d2c3bb7615745513216ebb7
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-03 22:23:25 -0500

    2.Native Features 6. Accessing the Library

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
-import React from "react";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useState, useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+import Screen from "./app/components/Screen";
+import { Button, Image } from "react-native";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    //const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION);
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) alert("You need to give permission to acces to CameraRoll!");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (ex) {
+      console.log("Error reading a image: ", ex);
+    }
+  };
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage}></Button>
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
