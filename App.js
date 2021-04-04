1186d1125b227259e7b2ec5e92673c564179d79c
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-04 00:18:35 -0500

    2.Native Features 10. Building ImageInputList - Basics

--- App.js
+++ App.js
@@ -1,37 +1,25 @@
-import React, { useState, useEffect } from "react";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
+import React, { useState } from "react";
 
 import Screen from "./app/components/Screen";
-import { Button, Image } from "react-native";
-import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
+  const [imageUris, setImageUris] = useState([]);
 
-  const requestPermission = async () => {
-    //const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION);
-    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-    if (!granted) alert("You need to give permission to acces to CameraRoll!");
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
   };
 
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri);
-    } catch (ex) {
-      console.log("Error reading a image: ", ex);
-    }
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
   };
   return (
     <Screen>
-      <Button title="Select Image" onPress={selectImage}></Button>
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-      <ImageInput imageUri={imageUri} />
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
     </Screen>
   );
 }
--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,17 +1,60 @@
-import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import React, { useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  Alert,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons/";
+import * as ImagePicker from "expo-image-picker";
 
 import colors from "../config/colors";
 
 function ImageInput({ imageUri, onChangeImage }) {
+  const requestPermission = async () => {
+    //const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION);
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) alert("You need to give permission to acces to CameraRoll!");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else
+      Alert.alert("Delete", "Are you sure you want to delete this image?", [
+        { text: "Yes", onPress: () => onChangeImage(null) },
+        { text: "No" },
+      ]);
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!result.cancelled) onChangeImage(result.uri);
+    } catch (ex) {
+      console.log("Error reading a image: ", ex);
+    }
+  };
   return (
-    <View style={styles.container}>
-      {!imageUri && (
-        <MaterialCommunityIcons name="camera" size={40} color={colors.medium} />
-      )}
-      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            name="camera"
+            size={40}
+            color={colors.medium}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
