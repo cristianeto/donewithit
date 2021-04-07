559fbed021f88f5b123ba5b684540dfdf928fc94
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-07 18:00:01 -0500

    3. Navigation 3. Stack Navigator

--- App.js
+++ App.js
@@ -1,7 +1,40 @@
 import React, { useState } from "react";
+import { Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
+
+const Tweets = () => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+    </Screen>
+  );
+};
+const TweetsDetails = () => {
+  return (
+    <Screen>
+      <Text>Tweets Details</Text>
+    </Screen>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="Tweets">
+      <Stack.Screen name="TweetsDetails" component={TweetsDetails} />
+      <Stack.Screen name="Tweets" component={Tweets} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
