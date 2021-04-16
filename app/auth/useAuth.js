cdd75386fe31dc2c8b986710bc9ee4aff2abf171
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-15 20:42:25 -0500

    Updating Lottie package
    - I fix the warning for Lottie, upgrade too version 3 of lottie
    - Also, Authentication. 14. Creating a custom hook

---
+++ app/auth/useAuth.js
@@ -0,0 +1,20 @@
+import jwtDecode from "jwt-decode";
+import { useContext } from "react";
+import AuthContext from "./context";
+import authStorage from "./storage";
+
+export default useAuth = () => {
+  const { user, setUser } = useContext(AuthContext);
+
+  const logIn = (authToken) => {
+    const user = jwtDecode(authToken);
+    setUser(user);
+    authStorage.storeToken(authToken);
+  };
+
+  const logOut = () => {
+    setUser(null);
+    authStorage.removetoken();
+  };
+  return { user, logIn, logOut };
+};
